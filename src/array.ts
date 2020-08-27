a1308f6a59a38f1e92ce5ef9df3677eb71d5ddca
yokubo <dcsu181111@gmail.com>
2020-08-28 02:06:07 +0900

    create new file

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {}
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto'];
+
+let strings: Array<string> = ['Typescript', 'JavaScript', 'CoffeeScript']
+
+let nijigenHaiaretsu: number[][] = [
+    [50, 100],
+    [150, 300]
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
