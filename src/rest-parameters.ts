a6610c00378c172848d943b8ee5b6ac60dc9caf3
yokubo <dcsu181111@gmail.com>
2020-08-30 23:07:50 +0900

    end of section3

---
+++ src/rest-parameters.ts
@@ -0,0 +1,14 @@
+export {};
+
+const reducer = (accumulator: number, currentValue: number) => {
+  console.log({ accumulator, currentValue });
+  return accumulator + currentValue;
+};
+
+const sum: (...values: number[]) => number = (
+  ...values: Array<number>
+): number => {
+  return values.reduce(reducer);
+};
+
+console.log(sum(1, 2, 3, 4, 5));
